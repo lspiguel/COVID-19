2069ee11aa392c1da6c06a14107f98bafb98dfd6
lspiguel <46658141+lspiguel@users.noreply.github.com>
2020-04-04 09:42:10 -0300

    Refactored into functions and a main

--- request-plot-dates.py
+++ request-plot-dates.py
@@ -6,12 +6,6 @@ import matplotlib
 import matplotlib.pyplot as plt
 from contextlib import closing
 
-confirmed_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
-countries_filter = ['Argentina','Spain','United Kingdom','Italy','US','Sweden','Brazil','Canada','China','Germany','Chile','India']
-#countries_filter = ['Argentina']
-threshold = 100
-data = {}
-
 class Date_Series():
 	def __init__(self, x):
 		self.x = x
@@ -35,6 +29,7 @@ class Over_Threshold_Series():
 	def SetConfirmed(self, values):		
 		x = []
 		y = []
+		threshold = 100
 		if values[0] < threshold:
 			j = 0
 			overthreshold = False
@@ -73,70 +68,87 @@ class Country_Data():
 		self.date_series.SetConfirmed(self.confirmed)
 		self.over_threshold_series.SetConfirmed(self.confirmed)
 
-with closing(requests.get(confirmed_url)) as r:
-	f = (line.decode('utf-8') for line in r.iter_lines())
-	reader = csv.reader(f, delimiter=',', quotechar='"')
-	headers = next(reader)[4:]
-	for row in reader:
-		name = row[1]
-		if name in countries_filter:
-			if (not name in data):
-				data[name] = Country_Data(name, headers)
-				data[name].SetConfirmed(row[4:])
-			else:
-				data[name].AddConfirmed(row[4:])
+def Load(url, filter):
+	with closing(requests.get(url)) as r:
+		return_data = {}
+		f = (line.decode('utf-8') for line in r.iter_lines())
+		reader = csv.reader(f, delimiter=',', quotechar='"')
+		headers = next(reader)[4:]
+		for row in reader:
+			name = row[1]
+			if name in filter:
+				if (not name in return_data):
+					return_data[name] = Country_Data(name, headers)
+					return_data[name].SetConfirmed(row[4:])
+				else:
+					return_data[name].AddConfirmed(row[4:])
+	return return_data
 
-linestyles = ['-','--','-.',':']
-fig1 = plt.figure(dpi=100)
-ax1 = plt.axes([0.1, 0.1, 0.8, 0.8]) #xticks=[], yticks=[]
-i = 0
-for name in countries_filter:
-	country_data = data[name]
-	if len(country_data.date_series.confirmed) > 0:
-		ax1.plot_date(country_data.date_series.x, country_data.date_series.confirmed,linestyles[(i//6)%4], label=country_data.name)
-		i += 1
+def Graph__Daily_Confirmed(data, filter):
+	linestyles = ['-','--','-.',':']
+	fig1 = plt.figure(dpi=100)
+	ax1 = plt.axes([0.1, 0.1, 0.8, 0.8]) #xticks=[], yticks=[]
+	i = 0
+	for name in filter:
+		country_data = data[name]
+		if len(country_data.date_series.confirmed) > 0:
+			ax1.plot_date(country_data.date_series.x, country_data.date_series.confirmed,linestyles[(i//6)%4], label=country_data.name)
+			i += 1
 		
-plt.title('Confirmed daily cases')
-ax1.legend(loc=2)
-#ax1.set_xlim(left=matplotlib.dates.date2num(datetime.date.today()))
-plt.show()
+	plt.title('Confirmed daily cases')
+	ax1.legend(loc=2)
+	#ax1.set_xlim(left=matplotlib.dates.date2num(datetime.date.today()))
+	plt.show()
+	plt.close(fig1)
 
-linestyles = ['-','--','-.',':']
-fig2 = plt.figure(dpi=100) #figsize=(16,8),dpi=200
-ax2 = plt.axes([0.1, 0.1, 0.8, 0.8])
-i = 0
-for name in countries_filter:
-	country_data = data[name]
-	if len(country_data.over_threshold_series.confirmed) > 0:
-		ax2.plot(country_data.over_threshold_series.x, country_data.over_threshold_series.confirmed, label=country_data.name,ls=linestyles[(i//6)%4],marker='')
-		i += 1
+def Graph_Over_Threshold(data, filter):
+	linestyles = ['-','--','-.',':']
+	fig2 = plt.figure(dpi=100) #figsize=(16,8),dpi=200
+	ax2 = plt.axes([0.1, 0.1, 0.8, 0.8])
+	i = 0
+	for name in filter:
+		country_data = data[name]
+		if len(country_data.over_threshold_series.confirmed) > 0:
+			ax2.plot(country_data.over_threshold_series.x, country_data.over_threshold_series.confirmed, label=country_data.name,ls=linestyles[(i//6)%4],marker='')
+			i += 1
 
-plt.title('Confirmed case progression per day after 100 cases')
-ax2.legend(loc=2)
-ax2.set_ylim(top=4000,bottom=0)
-ax2.set_xlim(right=20,left=0)
+	plt.title('Confirmed case progression per day after 100 cases')
+	ax2.legend(loc=2)
+	ax2.set_ylim(top=4000,bottom=0)
+	ax2.set_xlim(right=20,left=0)
+	plt.show()
+	plt.close(fig2)
 
-plt.show()
+def Graph_Bar(data, filter):
+	colors = ['b','g','r','c','m','y','k']
+	fig3 = plt.figure(dpi=100) #figsize=(16,8),dpi=200
+	ax3 = plt.axes([0.1, 0.1, 0.8, 0.8])
+	i = 0
+	for name in filter:
+		country_data = data[name]
+		if len(country_data.over_threshold_series.confirmed_delta) > 0:
+			x = [x + i / 7 for x in country_data.over_threshold_series.x]
+			ax3.bar(x, country_data.over_threshold_series.confirmed_delta, label=country_data.name,align='center',width=.15,color=colors[i%7],edgecolor='w')
+			i += 1
+		if i >= 6:
+			break
+	plt.title('Confirmed case difference per day after 100 cases')
+	ax3.legend(loc=2)
+	ax3.set_ylim(top=2000,bottom=0)
+	ax3.set_xlim(right=20,left=0)
+	plt.show()
+	plt.close(fig3)
 
-colors = ['b','g','r','c','m','y','k']
-fig3 = plt.figure(dpi=100) #figsize=(16,8),dpi=200
-ax3 = plt.axes([0.1, 0.1, 0.8, 0.8])
-i = 0
-for name in countries_filter:
-	country_data = data[name]
-	if len(country_data.over_threshold_series.confirmed_delta) > 0:
-		x = [x + i / 7 for x in country_data.over_threshold_series.x]
-		ax3.bar(x, country_data.over_threshold_series.confirmed_delta, label=country_data.name,align='center',width=.15,color=colors[i%7],edgecolor='w')
-		i += 1
-	if i >= 6:
-		break
+def main():
+	confirmed_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+	countries_filter = ['Argentina','Spain','United Kingdom','Italy','US','Sweden','Brazil','Canada','China','Germany','Chile','India']
+	#countries_filter = ['Argentina']
 
-plt.title('Confirmed case difference per day after 100 cases')
-ax3.legend(loc=2)
-ax3.set_ylim(top=2000,bottom=0)
-ax3.set_xlim(right=20,left=0)
-plt.show()
+	countries_data = Load(confirmed_url, countries_filter)
+	Graph__Daily_Confirmed(countries_data, countries_filter)
+	Graph_Over_Threshold(countries_data, countries_filter)
+	Graph_Bar(countries_data, countries_filter)
 
-plt.close(fig1)
-plt.close(fig2)
-plt.close(fig3)
+if __name__ == '__main__':
+	main()
+
