309e14cbb8e5f03f9ee07ea78b0e2d0df3e990b0
lspiguel <46658141+lspiguel@users.noreply.github.com>
2020-04-04 22:34:33 -0300

    WIP: added calculation and graph for doubling times

--- request-plot-dates.py
+++ request-plot-dates.py
@@ -1,6 +1,7 @@
 import requests
 import csv
 import datetime
+import math
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
@@ -29,9 +30,11 @@ class Date_Series():
 
 class Over_Threshold_Series():
 	def __init__(self):
+		self.threshold = 100
 		self.confirmed_x = np.array([])
 		self.confirmed = np.array([])
 		self.confirmed_delta = np.array([])
+		self.confirmed_doubling_time = np.array([])
 		self.recovered_x = np.array([])
 		self.recovered = np.array([])
 		self.deaths_x = np.array([])
@@ -39,12 +42,11 @@ class Over_Threshold_Series():
 	def SetConfirmed(self, values):		
 		x = []
 		y = []
-		threshold = 100
-		if values[0] < threshold:
+		if values[0] < self.threshold:
 			j = 0
 			overthreshold = False
 			for i in range(len(values)):
-				if int(values[i]) >= threshold or overthreshold == True:
+				if int(values[i]) >= self.threshold or overthreshold == True:
 					overthreshold = True
 				if overthreshold == True:
 					x.append(j)
@@ -56,12 +58,11 @@ class Over_Threshold_Series():
 	def SetRecovered(self, values):		
 		x = []
 		y = []
-		threshold = 100
-		if values[0] < threshold:
+		if values[0] < self.threshold:
 			j = 0
 			overthreshold = False
 			for i in range(len(values)):
-				if int(values[i]) >= threshold or overthreshold == True:
+				if int(values[i]) >= self.threshold or overthreshold == True:
 					overthreshold = True
 				if overthreshold == True:
 					x.append(j)
@@ -72,12 +73,11 @@ class Over_Threshold_Series():
 	def SetDeaths(self, values):		
 		x = []
 		y = []
-		threshold = 100
-		if values[0] < threshold:
+		if values[0] < self.threshold:
 			j = 0
 			overthreshold = False
 			for i in range(len(values)):
-				if int(values[i]) >= threshold or overthreshold == True:
+				if int(values[i]) >= self.threshold or overthreshold == True:
 					overthreshold = True
 				if overthreshold == True:
 					x.append(j)
@@ -91,7 +91,18 @@ class Over_Threshold_Series():
 			deltas.append(0)
 			for i in range(1, len(self.confirmed)):
 				deltas.append(self.confirmed[i] - self.confirmed[i - 1])
+		doublings = []
+		if len(deltas) > 3:
+			doublings.append(float("inf"))
+			doublings.append(float("inf"))
+			for i in range(2, len(self.confirmed)):
+				avg = float(deltas[i - 2] + deltas[i - 1] + deltas[i]) / 3
+				if avg != 0:
+					doublings.append(float(self.confirmed[i]) / avg)
+				else:
+					doublings.append(float("inf"))
 		self.confirmed_delta = np.array(deltas)
+		self.confirmed_doubling_time = np.array(doublings)
 			
 class Country_Data():
 	def __init__(self, country, datesStringList):
@@ -213,7 +224,7 @@ def Graph_Daily_Confirmed_Lineal(data, filter):
 			ax1.plot_date(country_data.date_series.x, country_data.date_series.confirmed,linestyles[(i//6)%4], label=country_data.name)
 			i += 1
 		
-	plt.title('Confirmed daily cases')
+	plt.title('Total confirmed cases, by date')
 	ax1.legend(loc=2)
 	#ax1.set_xlim(left=matplotlib.dates.date2num(datetime.date.today()))
 	plt.show()
@@ -230,7 +241,7 @@ def Graph_Daily_Deaths_Lineal(data, filter):
 			ax1.plot_date(country_data.date_series.x, country_data.date_series.deaths,linestyles[(i//6)%4], label=country_data.name)
 			i += 1
 		
-	plt.title('Daily deaths')
+	plt.title('Total deaths, by date')
 	ax1.legend(loc=2)
 	#ax1.set_xlim(left=matplotlib.dates.date2num(datetime.date.today()))
 	plt.show()
@@ -283,7 +294,7 @@ def Graph_Deaths_Over_Threshold_Log(data, filter):
 			ax2.plot(country_data.over_threshold_series.deaths_x, country_data.over_threshold_series.deaths, label=country_data.name,ls=linestyles[(i//6)%4],marker='')
 			i += 1
 
-	plt.title('Death toll per day after 100 deaths (log scale)')
+	plt.title('Total deaths per day after 100 deaths (log scale)')
 	ax2.legend(loc=2)
 	ax2.set_ylim(bottom=0,top=20000)
 	ax2.set_xlim(right=20,left=0)
@@ -310,6 +321,25 @@ def Graph_Confirmed_Deltas_Bar(data, filter):
 	plt.show()
 	plt.close(fig3)
 
+def Graph_Doubling_Time_Over_Threshold_Lineal(data, filter):
+	linestyles = ['-','--','-.',':']
+	fig2 = plt.figure(dpi=100) #figsize=(16,8),dpi=200
+	ax2 = plt.axes([0.1, 0.1, 0.8, 0.8])
+	i = 0
+	for name in filter:
+		country_data = data[name]
+		if len(country_data.over_threshold_series.confirmed_doubling_time) > 0:
+			ax2.plot(country_data.over_threshold_series.confirmed_x, country_data.over_threshold_series.confirmed_doubling_time, label=country_data.name,ls=linestyles[(i//6)%4],marker='')
+			i += 1
+
+	plt.title('Confirmed case doubling time in days, per day after 100 cases (3 day average)')
+	ax2.legend(loc=2)
+	ax2.set_ylim(bottom=0,top=20)
+	ax2.set_xlim(right=20,left=0)
+	plt.show()
+	plt.close(fig2)
+
+
 def main():
 	confirmed_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
 	deaths_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'
@@ -324,6 +354,7 @@ def main():
 	Graph_Confirmed_Over_Threshold_Log(countries_data, countries_filter)
 	Graph_Deaths_Over_Threshold_Log(countries_data, countries_filter)
 	#Graph_Confirmed_Deltas_Bar(countries_data, countries_filter)
+	Graph_Doubling_Time_Over_Threshold_Lineal(countries_data, countries_filter)
 
 if __name__ == '__main__':
 	main()
